e7dab790b12b9499d98dfdab9b0653fa05f0c2bd
Denis Buryi <denis@buryi.ru>
2023-09-14 12:07:54 +0700

    login page

--- moex/settings.py
+++ moex/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': ['templates'],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
